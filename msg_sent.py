4493275e26da3bd2abacbdb435118818881b45c3
Abila Khan <khankeya961@gmail.com>
2026-05-10 05:29:23 +0600

    Update msg_sent.py

--- msg_sent.py
+++ msg_sent.py
@@ -1,12 +1,21 @@
 import pywhatkit
 from datetime import datetime ,timedelta
+import time
+import tkinter as tk
+from tkinter import messagebox , ttk
 
-now= datetime.now()
 
-# Set the time for sending the message (e.g., 3 minute from now)
-send_time= now+ timedelta(minutes=3)
 
-print("Message will be sent at:", send_time.strftime("%H:%M"))
-# Send the message at the specified time
-pywhatkit.sendwhatmsg("+880 1704-180211", "Hello! This is a scheduled",
-                       send_time.hour, send_time.minute)
+Contact_number=[]
+
+
+#for dashboard
+def dashboard():
+    root = tk.Tk()
+    root.title("Whatsapp Bot System")
+    root.geometry("400x400")
+    root.configure(bg="#07a61f")
+    
+    root.mainloop()
+
+dashboard()
