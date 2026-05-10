47c7fc4c2dc320d7e36ee5f09ed0b3e33e639756
Abila Khan <khankeya961@gmail.com>
2026-05-10 23:28:59 +0600

    Update msg_sent.py

--- msg_sent.py
+++ msg_sent.py
@@ -35,10 +35,11 @@ def show_saved_contacts():
 def listbox():
     next_page1.pack_forget()
     next_page2.pack(fill="both", expand=True)
-    listbox = tk.Listbox(next_page2, font=("Arial", 12), width=30, height=10)
-    listbox.pack(pady=10)
+
+    listbox.delete(0, tk.END)
+
     for i, contact in enumerate(Contact_number):
-        listbox.insert(tk.END, f"{Contact_name[i]}: {contact}")
+        listbox.insert(tk.END, f"{Contact_name[i]} : {contact}")
 
 
 
@@ -102,17 +103,17 @@ contact_entry_name.pack(pady=10)
 
 # placeholder for contact name entry
 
-placeholder_text = "Enter contact name"
-contact_entry_name.insert(0, placeholder_text)
+name_placeholder = "Enter contact name"
+contact_entry_name.insert(0, name_placeholder)
 
 def on_entry_click(event):
-    if contact_entry_name.get()== placeholder_text:
+    if contact_entry_name.get()== name_placeholder:
         contact_entry_name.delete(0, tk.END)
         contact_entry_name.config(fg="black")
 
 def on_focusout(event):
     if contact_entry_name.get()== "":
-        contact_entry_name.insert(0, placeholder_text)
+        contact_entry_name.insert(0, name_placeholder)
         contact_entry_name.config(fg="gray")
 
 contact_entry_name.bind("<FocusIn>", on_entry_click)
@@ -142,17 +143,17 @@ tk.Label(
 contact_entry = tk.Entry(next_page1, font=("Arial", 14), width=30, fg="gray")
 contact_entry.pack(pady=10)
 
-placeholder_text = "Enter contact number"
-contact_entry.insert(0, placeholder_text)
+number_placeholder = "Enter contact number"
+contact_entry.insert(0, number_placeholder)
 
 def on_entry_click(event):
-    if contact_entry.get()== placeholder_text:
+    if contact_entry.get()== number_placeholder:
         contact_entry.delete(0, tk.END)
         contact_entry.config(fg="black")
 
 def on_focusout(event):
     if contact_entry.get()== "":
-        contact_entry.insert(0, placeholder_text)
+        contact_entry.insert(0, number_placeholder)
         contact_entry.config(fg="gray")
 
 contact_entry.bind("<FocusIn>", on_entry_click)
@@ -170,17 +171,19 @@ tk.Button(next_page1, text="Back", font=("Arial", 12), bg="#056410", fg="white",
 
 
 #page 3
-tk.Label(next_page2, text="saved contact number", font=("Arial", 14, "bold"), bg="#000000", fg="#048a18").pack(pady=20)
+
+tk.Label(next_page2, text="saved contact number", font=("Arial", 14, "bold"), bg="#000000",
+          fg="#048a18").pack(pady=20)
+
+listbox = tk.Listbox(next_page2, font=("Arial", 12), width=30, height=10,selectmode="multiple")
+listbox.pack(pady=10)
+
+
+
 
 tk.Button(next_page2, text="Back", font=("Arial", 12), bg="#056410", fg="white",
           command=lambda: [next_page2.pack_forget(),
                            next_page1.pack(fill="both", expand=True)]).pack(pady=10)
-
-
-
-
-
-
 
 
 
