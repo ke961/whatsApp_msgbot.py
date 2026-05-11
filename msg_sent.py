8d6d5df1f95915aef57d47d3fed28aa37100559d
Abila Khan <khankeya961@gmail.com>
2026-05-11 22:46:20 +0600

    Update msg_sent.py

--- msg_sent.py
+++ msg_sent.py
@@ -53,6 +53,7 @@ root.configure(bg="#000000")
 welcome_page = tk.Frame(root, bg="#000000")
 next_page1 = tk.Frame(root, bg="#000000")
 next_page2 = tk.Frame(root, bg="#000000")
+next_page3 = tk.Frame(root, bg="#000000")
 
 
 
@@ -175,9 +176,23 @@ tk.Button(next_page1, text="Back", font=("Arial", 12), bg="#056410", fg="white",
 tk.Label(next_page2, text="saved contact number", font=("Arial", 14, "bold"), bg="#000000",
           fg="#048a18").pack(pady=20)
 
-listbox = tk.Listbox(next_page2, font=("Arial", 12), width=30, height=10,selectmode="multiple")
+listbox = tk.Listbox(next_page2, font=("Arial", 12), width=30, height=10,selectmode="single",
+                      bg="#EDE5E5", fg="#048a18", highlightbackground="#048a18", highlightcolor="#048a18"
+                      , highlightthickness=2)
 listbox.pack(pady=10)
 
+
+#for selecting contact from listbox
+def on_select(event):
+    selected_index= listbox.curselection()
+    if selected_index:
+        selected_contact = listbox.get(selected_index)
+
+        next_page2.pack_forget()
+        next_page3.pack(fill="both", expand=True)
+
+        
+listbox.bind("<<ListboxSelect>>", on_select)
 
 
 
@@ -186,7 +201,43 @@ tk.Button(next_page2, text="Back", font=("Arial", 12), bg="#056410", fg="white",
                            next_page1.pack(fill="both", expand=True)]).pack(pady=10)
 
 
+#page 4
+tk.Label(next_page3, text="Write your Message", font=("Arial", 14, "bold"), bg="#000000",
+          fg="#048a18").pack(pady=20)
+entry_message = tk.Entry(next_page3, font=("Arial", 12), width=40, fg="Black")
+entry_message.pack(pady=10)
 
+
+
+def send_message():
+    selected_index= listbox.curselection()
+    if selected_index:
+        selected_index = selected_index[0]
+        selected_contact = Contact_number[selected_index]
+        message = entry_message.get()
+        if message:
+            pywhatkit.sendwhatmsg_instantly(
+                selected_contact,
+                message,
+                wait_time=20,
+                tab_close=True
+            )
+            messagebox.showinfo("Success", "Message sent successfully!")
+        else:
+            messagebox.showwarning("Input Error", "Please enter a message to send.")
+    else:
+        messagebox.showwarning("Selection Error", "Please select a contact from the list.")
+
+
+tk.Button(next_page3, text="Send Message", font=("Arial", 12), bg="#056410", fg="white", command=send_message).pack(pady=10)
+
+    
+tk.Button(next_page3, text="Back", font=("Arial", 12), bg="#056410", fg="white",
+          
+            command=lambda: [next_page3.pack_forget(),
+                             next_page2.pack(fill="both", expand=True)]).pack(pady=10)
+                                
+    
 
 #  RUN 
 root.mainloop()
